a3ffb3634efe9384ce8652c2000029846517c1ae
Nam Duong <phamnamduong583@gmail.com>
2024-10-06 17:03:02 +0700

    feat/search tweet in tweet circle and follow

--- src/models/requests/Search.requests.ts
+++ src/models/requests/Search.requests.ts
@@ -1,5 +1,8 @@
+import { MediaTypeQuery, PeopleFollow } from '~/constants/enums'
 import { Pagination } from './Tweet.requests'
 
 export interface SearchQuery extends Pagination {
   content: string
+  media_type?: MediaTypeQuery
+  people_follow?: PeopleFollow
 }
