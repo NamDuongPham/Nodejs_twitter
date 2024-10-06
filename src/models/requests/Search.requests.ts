e627cf492c7275aae7577b669cd9e9930ef13527
Nam Duong <phamnamduong583@gmail.com>
2024-10-06 17:39:44 +0700

    update search validator

--- src/models/requests/Search.requests.ts
+++ src/models/requests/Search.requests.ts
@@ -1,7 +1,7 @@
 import { MediaTypeQuery, PeopleFollow } from '~/constants/enums'
 import { Pagination } from './Tweet.requests'
-
-export interface SearchQuery extends Pagination {
+import { Query } from 'express-serve-static-core'
+export interface SearchQuery extends Pagination, Query {
   content: string
   media_type?: MediaTypeQuery
   people_follow?: PeopleFollow
--- src/routes/search.routes.ts
+++ src/routes/search.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express'
 import { searchController } from '~/controllers/searchs.controller'
+import { searchValidator } from '~/middlewares/search.middleware'
 import { paginationValidator } from '~/middlewares/tweets.middleware'
 import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/users.middlewares'
 
 const searchRouter = Router()
 searchRouter.get(
   '/',
-  // accessTokenValidator,
-  // verifiedUserValidator,
-  // // searchValidator,
-  // paginationValidator,
+  accessTokenValidator,
+  verifiedUserValidator,
+  searchValidator,
+  paginationValidator,
   searchController
 )
 export default searchRouter
