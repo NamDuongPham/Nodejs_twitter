9af29a7c216abae817eacc3bae99d886bab0ce67
Nam Duong <phamnamduong583@gmail.com>
2024-10-23 11:17:49 +0700

    create and connect s3 bucket aws

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import { initFolder } from './utils/file'
 import conversationsRouter from './routes/conversations.routes'
 import { createServer } from 'http'
 import initSocket from './utils/socket'
+import '~/utils/s3'
 config()
 databaseService.connect().then(() => {
   databaseService.indexUsers()
